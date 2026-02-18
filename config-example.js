0e977549028a24d9bfdde116365dc032bacd9d82
javier ferreño upm <javifc67@gmail.com>
2026-02-18 16:34:32 +0100

    updated config example

--- config-example.js
+++ config-example.js
@@ -2,7 +2,7 @@
 
 export let ESCAPP_APP_SETTINGS = {
   //Settings that can be specified by the authors
-  skin: "FUTURISTIC",
+  skin: "STANDARD", //can be STANDARD, RETRO or FUTURISTIC
   //Settings that will be automatically specified by the Escapp server
   // locale: "es", //if locale is undefined, you can set locale in app
 
